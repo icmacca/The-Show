f0d65a091ebfc6c658842276b1fea3614639c8bd
Todd <toddsandford@gmail.com>
2018-11-23 21:19:04 +1300

    Creating some structure

---
+++ src/main.py
@@ -0,0 +1,19 @@
+DEBUG = True
+
+
+def main(debug_mode):
+    """
+    Summary: 
+        Entry point of program. 
+        If debug mode then test code is run, otherwise
+        main program is run
+
+    Params:
+        Takes global debug as bool
+
+    """
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main(DEBUG)
